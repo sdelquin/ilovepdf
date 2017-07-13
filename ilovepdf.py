2a2f7c1471ce8838cdf6f8cb198b48fc9d5db3bd
Sergio Delgado Quintero <sdelquin@gmail.com>
2017-07-13 18:50:58 +0100

    Added colorlog to console

--- ilovepdf.py
+++ ilovepdf.py
@@ -1,11 +1,13 @@
 import jwt
 import requests
+import logging
+import logging.config
 
 API_ENTRY_POINT = "https://api.ilovepdf.com/v1/start"
 
 
 class ILovePdf:
-    def __init__(self, public_key, secret_key):
+    def __init__(self, public_key, secret_key, verbose=False):
         """
         You must register in order to get the keys.
         public_key: It can be obtained from
@@ -14,6 +16,11 @@ class ILovePdf:
         secret_key: It can be obtained from
                     https://developer.ilovepdf.com/user/projects
         """
+        logging.config.fileConfig("logging.cfg")
+        self.logger = logging.getLogger()
+        self.verbose = verbose
+        if self.verbose:
+            self.logger.info("Generating the encoded signed public key...")
         self.public_key = public_key
         self.secret_key = secret_key
         signed_public_key = jwt.encode(
@@ -26,16 +33,22 @@ class ILovePdf:
     def new_task(self, task):
         self.task = task
         url = "{}/{}".format(API_ENTRY_POINT, task)
+        if self.verbose:
+            self.logger.info("Getting the server and task id...")
         response = requests.get(url, headers=self.headers).json()
         self.server = response["server"]
         self.task_id = response["task"]
         self.base_api_url = "https://{}/v1".format(self.server)
 
     def add_file(self, filename):
+        if self.verbose:
+            self.logger.info("Adding file '{}'".format(filename))
         self.filename = filename
         url = self.base_api_url + "/upload"
         params = {"task": self.task_id}
         files = {"file": open(filename, "rb")}
+        if self.verbose:
+            self.logger.info("Getting the server filename...")
         response = requests.post(
             url,
             params,
@@ -56,6 +69,8 @@ class ILovePdf:
                 }
             ]
         }
+        if self.verbose:
+            self.logger.info("Processing the uploaded files...")
         response = requests.post(url, json=params, headers=self.headers).json()
         self.filesize = response["filesize"]
         self.output_filesize = response["output_filesize"]
@@ -63,6 +78,8 @@ class ILovePdf:
 
     def download(self, output_filename=None):
         url = self.base_api_url + "/download/{}".format(self.task_id)
+        if self.verbose:
+            self.logger.info("Downloading output files...")
         response = requests.get(url, headers=self.headers)
         output_filename = output_filename or self.filename
         with open(output_filename, "wb") as output_file:
--- ipdf.py
+++ ipdf.py
@@ -1,22 +1,28 @@
 """iLovePDF
 
 Usage:
-    ipdf.py TASK [-o FILE] INPUT
+    ipdf.py TASK [-o FILE] [--verbose] INPUT
 
 Options:
-    -h --help   Show this screen
-    TASK        Task to perform: merge, split, compress, pdfjpg, imagepdf,
-                unlock, pagenumber, watermark, officepdf, repair, rotate,
-                protect, pdfa, validatepdfa, extract
-    -o FILE     Specify output file. If no file is provided, output file will
-                be overwritten
-    INPUT       Specify input file
+    -h --help       Show this screen
+    TASK            Task to perform: merge, split, compress, pdfjpg, imagepdf,
+                    unlock, pagenumber, watermark, officepdf, repair, rotate,
+                    protect, pdfa, validatepdfa, extract
+    -o FILE         Specify output file. If no file is provided, output file
+                    will be overwritten
+    INPUT           Specify input file
+    -v --verbose    Show messages
 """
 
 from docopt import docopt
 import sys
 from ilovepdf import ILovePdf
 import config
+import logging
+import logging.config
+
+logging.config.fileConfig("logging.cfg")
+logger = logging.getLogger()
 
 TASKS = ("merge", "split", "compress", "pdfjpg", "imagepdf", "unlock",
          "pagenumber", "watermark", "officepdf", "repair", "rotate", "protect",
@@ -28,15 +34,15 @@ arguments = docopt(__doc__, version="iLovePDF 1.0")
 
 task = arguments["TASK"]
 if task not in TASKS:
-    print("Chosen task '{}' is not available".format(task))
+    logger.error("Chosen task '{}' is not available".format(task))
     sys.exit()
 if task not in IMPLEMENTED_TASKS:
-    print("Chosen task '{}' is not yet implemented".format(task))
+    logger.error("Chosen task '{}' is not yet implemented".format(task))
     sys.exit()
 
 filename = arguments["INPUT"]
 output_filename = arguments["-o"] or filename
-i = ILovePdf(config.PUBLIC_KEY, config.SECRET_KEY)
+i = ILovePdf(config.PUBLIC_KEY, config.SECRET_KEY, arguments["--verbose"])
 i.new_task(task)
 i.add_file(filename)
 i.execute()
